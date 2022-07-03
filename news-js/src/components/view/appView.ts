2aae048d18c773d2a482a0f3bc8d8250f2ffd7f7
rincewizz <gleoros@gmail.com>
2022-07-03 15:58:04 +0300

    feat: migrate appView.js to TypeScript

---
+++ news-js/src/components/view/appView.ts
@@ -0,0 +1,25 @@
+import { NewsEverything, NewsSources, ResponseNewsEverything, ResponseNewsSources } from '../types';
+import News from './news/news';
+import Sources from './sources/sources';
+
+export class AppView {
+    private news: News;
+    private sources: Sources;
+
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    public drawNews(data: ResponseNewsEverything) {
+        const values: NewsEverything[] | [] = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
+
+    public drawSources(data: ResponseNewsSources) {
+        const values: NewsSources[] = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
