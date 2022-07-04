e54990cbec079b4517a8ffae1ca9e507937ada26
rincewizz <gleoros@gmail.com>
2022-07-04 12:14:28 +0300

    fix: change api server to work correctly on github pages

--- news-js/src/components/controller/appLoader.ts
+++ news-js/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '5a1ab8b8adb14433bfa629db69429b7f', // получите свой ключ https://newsapi.org/
         });
     }
